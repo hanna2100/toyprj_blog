77b92497db9a0b7c7c5cd8d8b200ef0ffb138a67
hanna2100 <hanna0497@naver.com>
2020-04-09 06:32:30 +0000

    postgresql was installed

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'hanna2100',
+        'PASSWORD': 'jhnr842651',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
