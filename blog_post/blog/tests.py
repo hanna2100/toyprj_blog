434ec974f5c17edf3264d2dc1a32deb39c3e5253
hanna2100 <hanna0497@naver.com>
2020-04-13 10:34:38 +0900

    fix error

--- blog_post/blog/tests.py
+++ blog_post/blog/tests.py
@@ -221,7 +221,7 @@ class TestView(TestCase):
 
         #태그가 있는지 확안해보기 위해
         post_card_000 = main_div.find('div', id='post-card-{}'.format(post_000.pk))
-        self.assertIn('django', post_card_000.text)
+        self.assertIn('#django', post_card_000.text)
 
 
 
